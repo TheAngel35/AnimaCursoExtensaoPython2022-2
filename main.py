396273a48c50c1bb1cc25aecf5b8fa2aba6dfe6b
TheAngel35 <116230031+TheAngel35@users.noreply.github.com>
2022-10-20 01:17:58 +0000

    Nome e idade

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #Meu primeiro projeto Python
-print("hola, mundo!")
+print("hola, mundo!\n")
+nome="Angel Santiago"
+idade=20
+print("Meu nome é "+nome+"\n")
+print("Minha idade é "+str(idade)+"\n")
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}\n".format(idade))
+#As duas informações
+print("Meu nome é {} e tenho {} anos\n".format(nome,idade))
